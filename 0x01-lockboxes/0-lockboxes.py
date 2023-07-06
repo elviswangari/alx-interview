f9e0ce2ef1d964223788c170a4a028e446eb7c52
elviswangari <elviswangare@gmail.com>
2023-07-06 15:04:25 +0300

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    function to check whether all the boxes have been opened or can they be
+    opened with the provided key
+    """
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    seque_line = [0]
+
+    while seque_line:
+        box = seque_line.pop(0)
+        for item in boxes[box]:
+            if item >= 0 and item < n and not visited[item]:
+                visited[item] = True
+                seque_line.append(item)
+
+    return all(visited)
