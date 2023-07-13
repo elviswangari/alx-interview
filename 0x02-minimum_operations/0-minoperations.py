594a07ee26a3a4caa98a98cb06cfb67695054c21
elviswangari <elviswangare@gmail.com>
2023-07-13 20:18:46 +0300

    minimal operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+Minimal Operations
+'''
+
+
+def minOperations(n):
+    '''
+    a script to check minimal operations to copy and past n number of times
+    ARGS:
+        one arg n
+
+    RETURNS:
+        the operation count
+    '''
+    if not isinstance(n, int) or n < 1:
+        return 0
+
+    ops_count = 0
+    clipboard = 0
+    done = 1
+
+    while done < n:
+        if n % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        else:
+            done += clipboard
+            ops_count += 1
+
+    return ops_count
+
